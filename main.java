c790c0c04b3cd17381229548dfe2be7273cda908
solomongo™ <80227002+solomongoGUI@users.noreply.github.com>
2022-09-28 01:34:57 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+/**
+   @author Daw120
+   9. Pide dos números por teclado al usuario: a y b. 
+      Deberá mostrarse true si ambos números son iguales y false en caso contrario.
+ */
+package com.solomongo.ejercicio9_hoja3;
+import java.util.Scanner;
+public class Ejercicio9_Hoja3 {
+public static void main(String[] args) {      
+Scanner teclado = new Scanner(System.in); // Crear objeto teclado
+// Declarar variables
+int na; 
+int nb;
+boolean resultado; 
+
+// ENTRADA 
+System.out.println("Numero a: "); na=teclado.nextInt(); 
+System.out.println("Numero b: "); nb=teclado.nextInt();
+
+// OPERACION con OPERADORES TERNARIOS
+resultado=na==nb?true:false;
+
+// SALIDA
+System.out.println("Son iguales?:  "+resultado);  
+    }
+}
